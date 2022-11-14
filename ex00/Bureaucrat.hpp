cf1d283aff0ba96225d54b541dff3a9a935df0e0
Nartex <garadraw@gmail.com>
2022-11-14 22:02:48 +0100

    Added template for ex00

---
+++ ex00/Bureaucrat.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsimeono <vsimeono@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 21:59:49 by vsimeono          #+#    #+#             */
+/*   Updated: 2022/11/14 22:02:21 by vsimeono         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Biro
+{
+	public:
+		Biro();
+		Biro(const Biro &src);
+		Biro(std::string name);
+	
+	
+		
+	
+};
